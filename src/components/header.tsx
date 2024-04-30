a47b7cf8fc38e3d1bd14d6e24fc6e90fe6101341
antoniobiasotti <abamorim36@gmail.com>
2024-04-30 11:25:15 -0300

    mod: app routing

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export function Header() {
     return (
-        <div className="p-4 font-extrabold text-2xl border-b-2 border-b-antoniobia">
+        <div className="flex p-4 font-extrabold text-2xl border-b-2 border-b-antoniobia">
             DevLinks
             {/* <hr></hr> */}
             <Link
