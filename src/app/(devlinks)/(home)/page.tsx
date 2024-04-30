6649dac90f4b0ee12e7535def9b63cc9149860d4
antoniobiasotti <abamorim36@gmail.com>
2024-04-30 15:42:24 -0300

    working: header and main

--- src/app/(devlinks)/(home)/page.tsx
+++ src/app/(devlinks)/(home)/page.tsx
@@ -5,18 +5,20 @@ export default async function Home() {
   await new Promise(resolve => setTimeout(resolve, 1000))
 
   return (
-    <div className="inline-flex flex-col justify-center items-center py-0 px-96">
-      <div className="flex flex-col justify-center items-center">
-        <h2>Sobre</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Error assumenda ab ratione doloribus ea debitis, a perferendis libero alias, in impedit sequi iste, fuga exercitationem tenetur mollitia ipsum dolores aperiam
-        </p>
-      </div>
-      <div className="flex flex-col justify-center items-center">
-        <h2>De onde veio a ideia?</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus voluptatibus quam iure blanditiis odio temporibus non veritatis fugit, odit nihil, quis labore excepturi, cupiditate impedit deleniti? Culpa qui similique soluta?
-        </p>
+    <div className="bg-zinc-200 flex justify-center items-center h-screen"> 
+      <div className="bg-zinc-800 px-10 py-5 flex-col justify-center items-center gap-5 inline-flex rounded">
+        <div className="w-96 h-44 flex-col items-center gap-2 inline-flex">
+          <h2 className="font-bold">Sobre</h2>
+          <p className="text-justify">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error assumenda ab ratione doloribus ea debitis, a perferendis libero alias, in impedit sequi iste, fuga exercitationem tenetur mollitia ipsum dolores aperiam
+          </p>
+        </div>
+        <div className="w-96 h-44 flex-col justify-start items-center gap-2 inline-flex">
+          <h2 className="font-bold">De onde veio a ideia?</h2>
+          <p className="text-justify">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus voluptatibus quam iure blanditiis odio temporibus non veritatis fugit, odit nihil, quis labore excepturi, cupiditate impedit deleniti? Culpa qui similique soluta?
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,14 +3,18 @@ import Image from 'next/image'
 
 export function Header() {
     return (
-        <div className="flex items-center justify-around p-4 font-extrabold text-2xl border-b-2 border-b-antoniobia">
+        <div className="flex items-center justify-around p-4 font-bold text-2xl border-b-2 border-b-antoniobia">
             DevLinks
             {/* <hr></hr> */}
             <Link
                 href="https://github.com/antoniobiasotti/devlinks"
+                target='blank'
             >
                 <Image
-                    src="/github.svg" width={65} height={65} alt=""
+                    src="/github.svg" 
+                    width={32} 
+                    height={32} 
+                    alt=""
                 >
                 </Image>
             </Link>
