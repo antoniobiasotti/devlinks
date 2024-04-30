3cb571973a97596fdb4c45322c7d4c89cc6dad4b
antoniobiasotti <abamorim36@gmail.com>
2024-04-30 11:11:34 -0300

    working: github display on header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,19 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
 export function Header() {
     return (
         <div className="p-4 font-extrabold text-2xl border-b-2 border-b-antoniobia">
             DevLinks
             {/* <hr></hr> */}
+            <Link
+                href="https://github.com/antoniobiasotti/devlinks"
+            >
+                <Image
+                    src="/public/" width={65} height={65} alt=""
+                >
+                </Image>
+            </Link>
         </div>
     )
 }
