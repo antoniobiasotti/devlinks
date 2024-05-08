e41bea1ef08f262af5e0c0f669b9ea9296909e92
Antonio <abamorim36@gmail.com>
2024-05-08 11:53:05 -0300

    fine adjs

--- src/app/(devlinks)/layout.tsx
+++ src/app/(devlinks)/layout.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from 'react'
 
 export default function HomeLayout({ children } : { children: ReactNode }) {
     return (
-        <div className="justify-center min-h-screen bg-slate-900 text-zinc-50 px-8 py-6 mx-auto">
+        <div className="min-h-screen bg-slate-800 text-zinc-50 px-8 py-6">
             {children}
         </div>   
     ) 
