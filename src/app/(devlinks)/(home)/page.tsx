ef8bb7ce196c8f79ba6b02d0b3a075b1353aa1b7
antoniobiasotti <abamorim36@gmail.com>
2024-05-09 10:52:32 -0300

    fine adjs

--- src/app/(devlinks)/(home)/page.tsx
+++ src/app/(devlinks)/(home)/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { Header } from "@/components/header";
 import { MainNavigationBar } from "@/components/Navbar";
 
--- src/app/(devlinks)/layout.tsx
+++ src/app/(devlinks)/layout.tsx
@@ -1,4 +1,3 @@
-import { Header } from '../../components/header'
 import { ReactNode } from 'react'
 
 export default function HomeLayout({ children } : { children: ReactNode }) {
