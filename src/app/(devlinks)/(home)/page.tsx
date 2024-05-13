15b8ab3f739585bf962454947db05c95f30b6c5f
antoniobiasotti <abamorim36@gmail.com>
2024-05-13 10:36:44 -0300

    working on tabs routes + changed layout

--- src/app/(devlinks)/(home)/page.tsx
+++ src/app/(devlinks)/(home)/page.tsx
@@ -1,6 +1,3 @@
-import { Header } from "@/components/header";
-import { MainNavigationBar } from "@/components/Navbar";
-import { SettingsTabs } from "@/components/SettingsTabs";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -9,10 +6,7 @@ export default async function Home() {
   await new Promise(resolve => setTimeout(resolve, 1000))
 
   return (
-    <main className="h-full flex gap-3 bg-zinc-950 rounded-lg justify-center py-6 border border-slate-500">
       <div className="space-y-8 justify-center">
-        <Header />
-        <SettingsTabs/>
         <div className="flex flex-col justify-center items-center font-light p-4">
           <div className="w-full h-full bg-zinc-800 px-10 py-5 flex-col justify-center items-center gap-2 inline-flex border border-zinc-700 rounded">
             <div className="w-96 flex-col items-center gap-2 inline-flex">
@@ -46,6 +40,5 @@ export default async function Home() {
           </div>
         </div>
       </div>
-    </main>
   );
 }
--- src/components/SettingsTabs/index.tsx
+++ src/components/SettingsTabs/index.tsx
@@ -11,46 +11,51 @@ export function SettingsTabs() {
     <Tabs.Root value={currentTab} onValueChange={setCurrentTab}>
       <Tabs.List className="mt-6 flex w-full justify-around gap-4 border-b border-zinc-800">
         <TabItem
-          value="tab1"
-          title="Algorithms"
-          isSelected={currentTab === 'tab1'}
-        />
+            value="tab1"
+            title="Home"
+            isSelected={currentTab === 'tab1'}
+          />
         <TabItem
           value="tab2"
-          title="APIs"
+          title="Algorithms"
           isSelected={currentTab === 'tab2'}
         />
         <TabItem
           value="tab3"
-          title="Animations"
+          title="APIs"
           isSelected={currentTab === 'tab3'}
         />
-        <TabItem value="tab4" title="Icons" isSelected={currentTab === 'tab4'} />
-        <TabItem value="tab5" title="Blogs" isSelected={currentTab === 'tab5'} />
         <TabItem
-          value="tab6"
-          title="Colors"
-          isSelected={currentTab === 'tab6'}
+          value="tab4"
+          title="Animations"
+          isSelected={currentTab === 'tab4'}
         />
+        <TabItem value="tab5" title="Icons" isSelected={currentTab === 'tab5'} />
+        <TabItem value="tab6" title="Blogs" isSelected={currentTab === 'tab6'} />
         <TabItem
           value="tab7"
-          title="Tools"
+          title="Colors"
           isSelected={currentTab === 'tab7'}
         />
         <TabItem
           value="tab8"
-          title="Fonts"
+          title="Tools"
           isSelected={currentTab === 'tab8'}
         />
         <TabItem
           value="tab9"
-          title="Ilustrations"
+          title="Fonts"
           isSelected={currentTab === 'tab9'}
         />
         <TabItem
           value="tab10"
-          title="UI"
+          title="Ilustrations"
           isSelected={currentTab === 'tab10'}
+        />
+        <TabItem
+          value="tab11"
+          title="UI"
+          isSelected={currentTab === 'tab11'}
         />
       </Tabs.List>
     </Tabs.Root>
