00b7030bce27bf08f7177fa51cdf8b21e107a2d1
antoniobiasotti <abamorim36@gmail.com>
2024-05-17 09:50:22 -0300

    setting up grid layout on sessions/page

--- src/app/(devlinks)/(home)/session/[title]/page.tsx
+++ src/app/(devlinks)/(home)/session/[title]/page.tsx
@@ -1,10 +1,10 @@
 export default function SessionTheme() {
      return (
-          <div className="h-full border border-zinc-500">
-               <div className="h-full grid grid-cols-3">
-                    <div className="h-full border-r border-zinc-500">oi</div>
-                    <div>oi</div>
-                    <div>oi</div>
+          <div className="h-full flex justify-center border border-zinc-500">
+               <div className="grid grid-cols-3 border border-zinc-500">
+                    <div className="h-full border border-x-zinc-200">oi</div>
+                    <div className="border border-x-zinc-200">oi</div>
+                    <div className="border border-x-zinc-200">oi</div>
                </div>
           </div>
      )
